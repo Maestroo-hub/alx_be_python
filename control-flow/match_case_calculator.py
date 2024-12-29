a909a4df14174e383a065a2b875f72aa9aac4288
maestroo-hub <fawazkelani44@gmail.com>
2024-12-29 11:38:38 +0100

    this is a python project

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -16,6 +16,6 @@ match operation:
             print("Error: Cannot divide by zero.")
         else:
             result = num1 / num2
-            print(result)
+            print(f"The result is {result}.")
     case _:
         print("Invalid operation! Please choose from +, -, *, or /.")
