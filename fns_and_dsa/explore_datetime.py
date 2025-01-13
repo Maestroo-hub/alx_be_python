d3ce54c6a555c9f2f6533887ad16b29f6fd82500
maestroo-hub <fawazkelani44@gmail.com>
2025-01-13 05:56:27 +0100

    this is a python project

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -2,35 +2,26 @@ import datetime
 
 
 def display_current_datetime():
-    """
-    Displays the current date and time in a readable format.
-    """
-    current_date = datetime.datetime.now()
-    formatted_date = current_date.strftime("%Y-%m-%d %H:%M:%S")
-    print(f"Current date and time: {formatted_date}")
+    current_datetime = datetime.datetime.now()
+
+    formatted_current_datetime = current_datetime.strftime("%Y-%m-%d %H:%M:%S")
+
+    print(f"Current date and time: {formatted_current_datetime}")
+    return formatted_current_datetime
 
 
-def calculate_future_date(days):
-    """
-    Calculates the future date by adding a given number of days to the current date.
-    :param days: Number of days to add
-    :return: Future date in "YYYY-MM-DD" format
-    """
-    current_date = datetime.datetime.now()
-    future_date = current_date + datetime.timedelta(days=days)
-    formatted_future_date = future_date.strftime("%Y-%m-%d")
-    print(f"Future date: {formatted_future_date}")
+def calculate_future_date():
+    days_to_add = int(input("Enter the number of days to add to the current date: "))
 
+    current_date = datetime.datetime.now().date()
 
-def main():
-    display_current_datetime()
+    future_date = current_date + datetime.timedelta(days=days_to_add)
 
-    try:
-        days_to_add = int(input("Enter the number of days to add to the current date: "))
-        calculate_future_date(days_to_add)
-    except ValueError:
-        print("Invalid input. Please enter an integer value for the number of days.")
+    print(f"Future date: {future_date}")
+    return future_date
 
 
 if __name__ == "__main__":
-    main()
+    display_current_datetime()
+    calculate_future_date()
+
